f05eae869c04020ed3f56f4d87669c74d90b7c41
Francesco Fico <fficusbenjamin@gmail.com>
2021-06-03 16:11:26 +0100

    favourite addedd

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
+// #docregion MyApp
 class MyApp extends StatelessWidget {
+  // #docregion build
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,57 +13,74 @@ class MyApp extends StatelessWidget {
       home: RandomWords(),
     );
   }
+// #enddocregion build
 }
+// #enddocregion MyApp
 
-class RandomWordsState extends State<RandomWords> {
+// #docregion RWS-var
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+  final _biggerFont = TextStyle(fontSize: 18.0);
+  // #enddocregion RWS-var
+
+  // #docregion _buildSuggestions
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+  // #enddocregion _buildSuggestions
+
+  // #docregion _buildRow
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+  // #enddocregion _buildRow
+
+  // #docregion RWS-build
   @override
-
-
   Widget build(BuildContext context) {
-    final List<WordPair> _suggestions = <WordPair>[];
-    final TextStyle _biggerFont = const TextStyle(fontSize: 18);
-
-    Widget _buildRow(WordPair pair) {
-      return ListTile(
-        title: Text(
-          pair.asPascalCase,
-          style: _biggerFont,
-        ),
-      );
-    }
-
-    Widget _buildSuggestions(){
-      return ListView.builder(padding: const EdgeInsets.all(16),
-      itemBuilder: (BuildContext_context, int i){
-
-        if (i.isOdd){return Divider();}
-
-        final int index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));}
-      return _buildRow(_suggestions[index]);
-
-
-      }
-      );
-
-
-    }
-
-
-    return Scaffold (                   // Add from here...
+    return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
       ),
       body: _buildSuggestions(),
     );
   }
+// #enddocregion RWS-build
+// #docregion RWS-var
 }
+// #enddocregion RWS-var
 
 class RandomWords extends StatefulWidget {
   @override
-  RandomWordsState createState() => RandomWordsState();
+  State<RandomWords> createState() => _RandomWordsState();
 }
-
-
-
